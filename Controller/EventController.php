46cf19707f3f7d3eb104b5828fb7ab0382a8b4de
mydz0 <stephanyarellano147@gmail.com>
2026-05-15 13:41:18 +0200

    Add EventController with methods to retrieve all events and a specific event by ID #35

---
+++ Controller/EventController.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once dirname(__FILE__) . '/../Model/db.php';
+
+class EventController 
+
+{
+    private PDO $connection;
+
+    //el contructor para conectarlo a la base de datos 
+    public function __construct()
+    {
+
+        $this->connection = Database::getInstance()->getConexion();
+
+    }
+
+    //LEER TODOS LOS EVENTOS: los devuelve todos 
+    public function getAllEvents(): array 
+    {
+        $sql = $this->connection->prepare($sql);
+        $stmt->execute();
+
+        $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $events;
+    }
+
+    //LEER UNO EN ESPECIFICO: los busca por ID 
+    public function getEventById(int $id): array|null
+    {
+        $sql = "SELECT * FROM events WHERE id = :id";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([':id' => $id]);
+
+        $event = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $event ?:null;
+
+    }
+}
